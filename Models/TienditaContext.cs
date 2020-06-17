68ae93471304a491198553416afcb3686b3eda61
Alejandro Poot <pootalejandro@yahoo.fr>
2020-06-17 13:03:57 -0500

    Agregar archivos de proyecto.

---
+++ Models/TienditaContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tiendita.Models
+{
+    class TienditaContext : DbContext
+    {
+        public DbSet<Producto> Productos { get; set; }
+        public DbSet<Venta> Ventas { get; set; }
+        public DbSet<Detalle> Detalles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("server=localhost;database=tiendita;user=root;password=");
+        }
+    }
+}
